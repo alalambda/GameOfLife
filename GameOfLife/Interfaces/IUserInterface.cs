677a7f280d9352629e3a3e297ee17d6212e37bb2
alalambda <annussaa@gmail.com>
2018-09-20 13:26:37 +0300

    method signatures in interface

--- GameOfLife/Interfaces/IUserInterface.cs
+++ GameOfLife/Interfaces/IUserInterface.cs
@@ -9,8 +9,10 @@ namespace GameOfLife.Interfaces
         int GetDimensionInput(string dimensionName);
         void LiveCellsOutput(int liveCells);
         void IterationsOutput(int iterations);
+        bool IsAnyKeyPressed();
         void GameOverOutput();
         void AskForTerminateGame();
         void AskForSaveGame();
+        bool IsGameSaveRequired();
     }
 }
